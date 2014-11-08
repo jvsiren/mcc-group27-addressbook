6fc0bfb91bdf6edd5ccdeafe1a7f6c72dd90ce06
Juha Siren <juha.siren@aalto.fi>
2014-11-08 15:03:09 +0200

    Export started

--- googleContactsService.js
+++ googleContactsService.js
@@ -2,34 +2,50 @@ var googleapis = require('googleapis');
 var OAuth2Client = googleapis.auth.OAuth2;
 var CLIENT_ID = '709847374314-ovdqsjq54bj4llbpr219rrg55dt1alop.apps.googleusercontent.com';
 var CLIENT_SECRET = '06KY_8EFUBsQvg_KceLvdI4T';
-var REDIRECT_URL = 'http://mccgroup27.ddns.net:8080/api/google/oauth2callback';
-var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var IMPORT_REDIRECT_URL = 'http://mccgroup27.ddns.net:8080/api/google/import/oauth2callback';
+var EXPORT_REDIRECT_URL = 'http://mccgroup27.ddns.net:8080/api/google/export/oauth2callback';
+var oauth2ClientImport = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, IMPORT_REDIRECT_URL);
+var oauth2ClientExport = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, EXPORT_REDIRECT_URL);
 var GoogleContacts = require('google-contacts').GoogleContacts;
 var contacts;
 var dao = require('./dao');
 var collectionName = 'contacts';
 
-var url = oauth2Client.generateAuthUrl({
-  access_type: 'offline', 
-  scope: 'https://www.googleapis.com/auth/contacts'
-});
+var importUrl = createUrl(oauth2ClientImport);
+var exportUrl = createUrl(oauth2ClientExport);
 
-exports.requestImportContacts = function(req, res) {
-    console.log(url);
-		res.redirect(url);	
+function createUrl(oauthClient) {
+  return oauthClient.generateAuthUrl({
+    access_type: 'offline', 
+    scope: 'https://www.googleapis.com/auth/contacts'
+  });
 };
 
-exports.oauthCallback = function(req, res) {
-	setAccessToken(req.query.code, function() { 
+exports.requestImportContacts = function(req, res) {
+		res.redirect(importUrl);	
+};
+
+exports.requestExportContacts = function(req, res) {
+  res.redirect(exportUrl);
+}
+
+exports.oauthCallbackImport = function(req, res) {
+	setAccessToken(oauth2ClientImport, req.query.code, function() { 
 		importContacts(res); 
 	});
 };
 
-function setAccessToken(authorizationCode, callback) {
-    oauth2Client.getToken(authorizationCode, function (err, tokens) {
+exports.oauthCallbackExport = function(req, res) {
+  setAccessToken(oauth2ClientExport, req.query.code, function() { 
+    exportContacts(res); 
+  });
+};
+
+function setAccessToken(oAuthClient, authorizationCode, callback) {
+    oAuthClient.getToken(authorizationCode, function (err, tokens) {
       // Now tokens contains an access_token and an optional refresh_token. Save them.
       if(!err) {
-        oauth2Client.setCredentials(tokens);
+        oAuthClient.setCredentials(tokens);
     		contacts = new GoogleContacts({token: tokens.access_token});
     		callback();
       }
@@ -72,3 +88,9 @@ function redirectToContactList(res) {
 	res.redirect('http://mccgroup27.ddns.net:8080');
 };
 
+function exportContacts(res) {
+  dao.findAll(collectionName, {}, function (allContacts) {
+    
+  });
+  redirectToContactList(res);
+}
