db8f8e27c994866cb815cde0e6c5d2c6f2f2a266
Juha Siren <juha.siren@aalto.fi>
2014-11-08 13:46:46 +0200

    Fixed typos

--- addressService.js
+++ addressService.js
@@ -15,25 +15,25 @@ function sendResponse(result, res) {
 };
 
 exports.findAllAddresses = function(req, res) {
-  dao.findAll(collectionName, {}, function (result) { sendReponse(result, res); });
+  dao.findAll(collectionName, {}, function (result) { sendResponse(result, res); });
 };
 
 exports.findAddressById = function(req, res) {
-  dao.findById(collectionName, req.params.id, function (result) { sendReponse(result, res); });
+  dao.findById(collectionName, req.params.id, function (result) { sendResponse(result, res); });
 };
 
 exports.findAddressesByQuery = function(req, res) {
-  dao.findAll(collectionName, req.body, function (result) { sendReponse(result, res); });
+  dao.findAll(collectionName, req.body, function (result) { sendResponse(result, res); });
 };
 
 exports.createAddress = function (req, res) {
-  dao.create(collectionName, req.body, function (result) { sendReponse(result, res); });
+  dao.create(collectionName, req.body, function (result) { sendResponse(result, res); });
 };
 
 exports.modifyAddress = function (req, res) {
-  dao.update(collectionName, req.params.id, req.body, function (result) { sendReponse(result, res); });
+  dao.update(collectionName, req.params.id, req.body, function (result) { sendResponse(result, res); });
 };
 
 exports.deleteAddress = function (req, res) {
-  dao.delete(collectionName, req.params.id, function (result) { sendReponse(result, res); });
+  dao.delete(collectionName, req.params.id, function (result) { sendResponse(result, res); });
 };
